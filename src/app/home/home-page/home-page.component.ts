e0ef510ebb17464653e2021af027a58cb5a6ad09
Mariana Martins <marih_ms@hotmail.com>
2024-04-11 13:21:44 -0300

    Create sidebar-menu
    Created the sidebar-menu component, with all navigation buttons, to be applied later on all pages, with the exception of the login page.

--- src/app/home/home-page/home-page.component.ts
+++ src/app/home/home-page/home-page.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SidebarMenuComponent } from '../../shared/sidebar-menu/sidebar-menu.component';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [],
+  imports: [SidebarMenuComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
