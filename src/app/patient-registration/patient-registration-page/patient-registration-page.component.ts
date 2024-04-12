f1f880a3e5401f5b1a693943be8d3b25720baf4f
Mariana Martins <marih_ms@hotmail.com>
2024-04-12 20:18:20 -0300

    Create buttons and formControl
    Created save, edit, and delete buttons. The buttons are not yet functional. Added formControl to the form elements for later manipulation.

--- src/app/patient-registration/patient-registration-page/patient-registration-page.component.ts
+++ src/app/patient-registration/patient-registration-page/patient-registration-page.component.ts
@@ -7,16 +7,43 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { MatNativeDateModule } from '@angular/material/core';
+import {MatButtonModule, MatButton} from '@angular/material/button';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 
 @Component({
   selector: 'app-patient-registration-page',
   standalone: true,
-  imports: [SidebarMenuComponent, ToolbarComponent, MatFormFieldModule, MatInputModule, MatSelectModule, MatFormField, MatDatepickerModule, MatNativeDateModule],
+  imports: [SidebarMenuComponent, ToolbarComponent, MatFormFieldModule, MatInputModule, MatSelectModule, MatFormField, MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatButton, ReactiveFormsModule],
   templateUrl: './patient-registration-page.component.html',
   styleUrl: './patient-registration-page.component.scss'
 })
 export class PatientRegistrationPageComponent implements OnInit {
+  patRegistration = new FormGroup({
+    name: new FormControl('',),
+    gender: new FormControl('',),
+    cpf: new FormControl('',),
+    rg: new FormControl('',),
+    issOrg: new FormControl('',),
+    maritalStatus: new FormControl('',),
+    phone: new FormControl('',),
+    placeOfBirth: new FormControl('',),
+    emergCont: new FormControl('',),
+    emergContNumber: new FormControl('',),
+    listOfAllergies: new FormControl('',),
+    careList: new FormControl('',),
+    healthInsurance: new FormControl('',),
+    healthInsuranceNumber: new FormControl('',),
+    healthInsuranceVal: new FormControl('',),
+    zipcode: new FormControl('',),
+    street: new FormControl('',),
+    addressNumber: new FormControl('',),
+    complement: new FormControl('',),
+    referencePoint: new FormControl('',),
+    neighborhood: new FormControl('',),
+    city: new FormControl('',),
+    state: new FormControl('',),
+  })
 
   constructor(private titleService: Title) { }
 
