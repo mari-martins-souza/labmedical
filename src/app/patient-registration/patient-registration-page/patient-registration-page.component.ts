5260fbf32f08d3d2a702c1f3ce785ed55c84a842
Mariana Martins <marih_ms@hotmail.com>
2024-04-13 11:48:10 -0300

    Change form save method from localStorage to JSON server
    Installed JSON server in the project, created db.json file. In the shared folder, created a services folder where the shared services will be located. Created data service to save information in db.json. DataService called on patient-registration-page to save inputs from patRegistration form. Changed savePatRegister function to call the service instead of saving in localStorage.

--- src/app/patient-registration/patient-registration-page/patient-registration-page.component.ts
+++ src/app/patient-registration/patient-registration-page/patient-registration-page.component.ts
@@ -12,18 +12,19 @@ import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { NgxMaskDirective, provideNgxMask, NgxMaskPipe } from 'ngx-mask';
 import { HttpClientModule } from '@angular/common/http';
 import { AddressService } from '../address.service';
+import { DataService } from '../../shared/services/data.service';
 
 @Component({
   selector: 'app-patient-registration-page',
   standalone: true,
   imports: [SidebarMenuComponent, ToolbarComponent, MatFormFieldModule, MatInputModule, MatSelectModule, MatFormField, MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatButton, ReactiveFormsModule, NgxMaskDirective, NgxMaskPipe, HttpClientModule],
-  providers: [provideNgxMask(), AddressService],
+  providers: [provideNgxMask(), AddressService, DataService],
   templateUrl: './patient-registration-page.component.html',
   styleUrl: './patient-registration-page.component.scss'
 })
 export class PatientRegistrationPageComponent implements OnInit {
   
-  constructor(private titleService: Title, private addressService: AddressService, private fb: FormBuilder) { }
+  constructor(private dataService: DataService, private titleService: Title, private addressService: AddressService, private fb: FormBuilder) { }
 
   patRegistration = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(8), Validators.maxLength(64)]],
@@ -79,11 +80,7 @@ export class PatientRegistrationPageComponent implements OnInit {
 
   savePatRegister() {
     if (this.patRegistration.valid) {
-        let patients = [];
-        const databasePatients = localStorage.getItem('databasePatients');
-        if (databasePatients !== null) {
-          patients = JSON.parse(databasePatients);
-        }
+        
         const patient = {
           name: this.patRegistration.value.name,
           gender: this.patRegistration.value.gender,
@@ -111,13 +108,13 @@ export class PatientRegistrationPageComponent implements OnInit {
           city: this.patRegistration.value.city,
           state: this.patRegistration.value.state,
         }
-        patients.push(patient);
-        localStorage.setItem('databasePatients', JSON.stringify(patients));
-        window.alert('Paciente registrado com sucesso.')
-        
-      } else {
-        window.alert('Preencha todos os campos obrigatórios corretamente.')
-    }
+
+        this.dataService.saveData('patients', patient).subscribe(() => {
+      window.alert('Paciente registrado com sucesso.')
+    });
+  } else {
+    window.alert('Preencha todos os campos obrigatórios corretamente.')
   }
+}
 
 }
