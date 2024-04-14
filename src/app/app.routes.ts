56563559ccfa9389e9dc9be6f6d5610f330d774e
Mariana Martins <marih_ms@hotmail.com>
2024-04-14 01:48:46 -0300

    Create exam registration form
    Created a form for exam registration. The form page performs a patient search and automatically fills in the patient’s id and name. Upon saving, the data is sent to the JSON Server and displays an animation upon save confirmation.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,10 +3,12 @@ import { LoginPageComponent } from './login/login-page/login-page.component';
 import { HomePageComponent } from './home/home-page/home-page.component';
 import { PatientRegistrationPageComponent } from './patient-registration/patient-registration-page/patient-registration-page.component';
 import { MedicalAppointmentRegPageComponent } from './medical-appoint-reg/medical-appointment-reg-page/medical-appointment-reg-page.component';
+import { ExamRegisterPageComponent } from './exam-register/exam-register-page/exam-register-page.component';
 
 export const routes: Routes = [
     { path: '', component: LoginPageComponent },
     { path: 'home', component: HomePageComponent },
     { path: 'registro-paciente', component: PatientRegistrationPageComponent },
     { path: 'registro-consulta', component: MedicalAppointmentRegPageComponent },
+    { path: 'registro-exame', component: ExamRegisterPageComponent },
 ];
---
+++ src/app/exam-register/exam-register-page/exam-register-page.component.ts
@@ -0,0 +1,127 @@
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
+import { DataTransformService } from '../../shared/services/data-transform.service';
+import { SidebarMenuComponent } from '../../shared/sidebar-menu/sidebar-menu.component';
+import { ToolbarComponent } from '../../shared/toolbar/toolbar.component';
+import { MatFormField, MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatButtonModule, MatButton } from '@angular/material/button';
+import { FormBuilder, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgxMaterialTimepickerModule } from 'ngx-material-timepicker';
+import { DataService } from '../../shared/services/data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { Observable, map, startWith } from 'rxjs';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+@Component({
+  selector: 'app-exam-register-page',
+  standalone: true,
+  imports: [ToolbarComponent, SidebarMenuComponent, MatFormFieldModule, MatInputModule, MatSelectModule, MatFormField, MatDatepickerModule, MatNativeDateModule, MatButtonModule, MatButton, ReactiveFormsModule, CommonModule, NgxMaterialTimepickerModule, HttpClientModule, MatAutocompleteModule],
+  providers: [DataTransformService, DataService],
+  templateUrl: './exam-register-page.component.html',
+  styleUrl: './exam-register-page.component.scss'
+})
+export class ExamRegisterPageComponent implements OnInit {
+  showMessage = false;
+  patients: any[] = [];
+  filteredPatients: Observable<any[]> | undefined;
+  patientSearchControl = new FormControl();
+
+  constructor(private dataTransformService: DataTransformService, private titleService: Title, private fb: FormBuilder, private dataService: DataService) { }
+
+  examRegister = this.fb.group({
+    id: [''],
+    name: [''],
+    exam: ['',[Validators.required, Validators.minLength(8), Validators.maxLength(64)]],
+    examDate: ['',Validators.required],
+    examTime: ['',Validators.required],
+    examType: ['',[Validators.required, Validators.minLength(4), Validators.maxLength(32)]],
+    lab: ['',[Validators.required, Validators.minLength(4), Validators.maxLength(32)]],
+    docUrl: ['',],
+    result: ['',[Validators.required, Validators.minLength(16), Validators.maxLength(1024)]],
+  })
+
+  ngOnInit() {
+
+    this.titleService.setTitle('Registro de Exame');
+    
+    const currentDate = new Date();
+    const dateString = currentDate.toISOString().split('T')[0];
+    const timeString = currentDate.getHours() + ':' + currentDate.getMinutes();
+  
+    this.examRegister.patchValue({
+      examDate: dateString,
+      examTime: timeString,
+    });   
+    
+    this.dataService.getData('patients').subscribe(data => {
+      this.patients = data;
+      this.filteredPatients = this.patientSearchControl.valueChanges
+        .pipe(
+          startWith(''),
+          map(value => typeof value === 'string' ? value : value.name),
+          map(name => name ? this._filter(name) : this.patients.slice())
+        );
+    });
+    
+  }
+
+  private _filter(name: string): any[] {
+    const filterValue = name.toLowerCase();
+    return this.patients.filter(patient => patient.name.toLowerCase().includes(filterValue));
+  }
+
+  setPatientData(patient: { id: any; name: any; }) {
+    this.examRegister.patchValue({
+      id: patient.id,
+      name: patient.name
+    });
+    this.patientSearchControl.setValue('');
+  }
+  
+  saveExamRegister() {
+    if (this.examRegister.valid) {
+        
+        const exam = {
+          id: this.examRegister.value.id,
+          name: this.examRegister.value.name,
+          exam: this.examRegister.value.exam,
+          examDate: this.dataTransformService.formatDate(this.examRegister.value.examDate),
+          examTime: this.examRegister.value.examTime,
+          examType: this.examRegister.value.examType,
+          lab: this.examRegister.value.lab,
+          docUrl: this.examRegister.value.docUrl,
+          result: this.examRegister.value.result,
+        }
+
+        this.dataService.saveData('exams', exam).subscribe(() => {
+          this.showMessage = true;
+
+          setTimeout(() => {
+            this.showMessage = false;
+          }, 1000);
+
+          const examDateControl = this.examRegister.get('examDate');
+          const examTimeControl = this.examRegister.get('examTime');
+
+          if (examDateControl && examTimeControl) {
+            this.examRegister.reset({
+              examDate: examDateControl.value,
+              examTime: examTimeControl.value
+          });
+  }
+    });
+  } else {
+    window.alert('Preencha todos os campos obrigatórios corretamente.')
+
+    }
+  }  
+  
+}
+
+
+
