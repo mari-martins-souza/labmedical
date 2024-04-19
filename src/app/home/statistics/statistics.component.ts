331798d51eda1493d217cbdd45d04f2cd04db738
Mariana Martins <marih_ms@hotmail.com>
2024-04-18 22:57:10 -0300

    Create statistics component and update styles
    A component was created to handle statistical data and included on the home page. The application layout has been updated.

---
+++ src/app/home/statistics/statistics.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+import { DataService } from '../../shared/services/data.service';
+import { HttpClientModule } from '@angular/common/http';
+
+@Component({
+  selector: 'app-statistics',
+  standalone: true,
+  imports: [MatCardModule, HttpClientModule],
+  providers: [DataService],
+  templateUrl: './statistics.component.html',
+  styleUrl: './statistics.component.scss'
+})
+export class StatisticsComponent implements OnInit {
+    countPatients: number = 0;
+    countAppointments: number = 0;
+    countExams: number = 0;
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit() {
+    this.dataService.countData('patients').subscribe(num => this.countPatients = num);
+    this.dataService.countData('appointments').subscribe(num => this.countAppointments = num);
+    this.dataService.countData('exams').subscribe(num => this.countExams = num);
+  }
+}
