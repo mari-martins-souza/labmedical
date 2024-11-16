8a0964f4a0f6bc8fd99b06a3ad87f63dd13e726f
mari-martins-souza <marih_ms@hotmail.com>
2024-11-16 13:34:42 -0300

    fix: age pipe

--- src/app/shared/pipes/age.pipe.ts
+++ src/app/shared/pipes/age.pipe.ts
@@ -6,12 +6,20 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class AgePipe implements PipeTransform {
 
-  transform(birthdate: string): number {
-    const [day, month, year] = birthdate.split('-').map(Number);
-    const birthdateAsDate = new Date(year, month - 1, day);
+  transform(birthdate: string | Date): number {
+    let birthdateAsDate: Date;
+  
+    if (typeof birthdate === 'string') {
+      birthdateAsDate = new Date(birthdate);
+    } else {
+      birthdateAsDate = birthdate;
+    }
+  
     const ageDifMs = Date.now() - birthdateAsDate.getTime();
+  
     const ageDate = new Date(ageDifMs);
+  
     return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
-
+  
 }
